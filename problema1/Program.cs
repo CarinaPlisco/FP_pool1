c917e6f763137bc2b4f442e42b8c2dedeb439845
Carina Rebeca <carina.plisco@gmail.com>
2020-12-18 16:21:38 +0200

    problema 1

---
+++ problema1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare.  
+
+            float a, b, x;
+
+            Console.Write("numarul a = ");
+            a = float.Parse(Console.ReadLine());
+            Console.Write("numarul b = ");
+            b = float.Parse(Console.ReadLine());
+
+            x = -b / a;
+
+            Console.WriteLine($" {a}x + {b} = 0 ");
+            Console.WriteLine($" => x = {x}");
+        }
+    }
+}
