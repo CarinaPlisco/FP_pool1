4f28cf8df4d4c91a3bbe2dbc29d6e6634c52e4f9
Carina Rebeca <carina.plisco@gmail.com>
2020-12-18 19:54:25 +0200

    problema3 commit

---
+++ problema3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determinati daca n se divide cu k, unde n si k sunt date de intrare. 
+
+            float n, k;
+
+            Console.WriteLine("n = ");
+            n = float.Parse(Console.ReadLine());
+            Console.WriteLine("k = ");
+            k = float.Parse(Console.ReadLine());
+
+            if(n % k == 0 )
+            {
+                Console.WriteLine($"{n} se divide cu {k}");
+            }
+            else
+            {
+                Console.WriteLine($"{n} nu se divide cu {k}");
+            }
+        }
+    }
+}
