8088de0184cf130b9a53f673e793cfeb4fa4b49a
Carina Rebeca <carina.plisco@gmail.com>
2020-12-18 23:53:08 +0200

    problema 5

---
+++ problema5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga. 
+            int n, k, uc, c;
+
+            Console.WriteLine("Introduceti un numar n: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A cata cifra de la sfarsitul numarului n doriti sa extrageti? ");
+            Console.WriteLine("k = ");
+            k = int.Parse(Console.ReadLine());
+
+            c = k;
+
+            do
+            {
+                uc = n % 10;
+                n = n / 10;
+                k--;
+            }
+            while (k > 0);
+            
+            Console.WriteLine($"A {c}-a cifra de la capat este {uc}");
+        }
+    }
+}
