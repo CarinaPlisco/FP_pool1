c1635737410700d8f46c9012212662f70fd48449
Carina Rebeca <carina.plisco@gmail.com>
2020-12-19 20:55:57 +0200

    problema 9 - divizorii lui n

---
+++ problema9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Afisati toti divizorii numarului n. 
+
+            int n, i;
+
+            Console.WriteLine("Introduceti un numar:");
+            Console.Write("n = ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                    Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
