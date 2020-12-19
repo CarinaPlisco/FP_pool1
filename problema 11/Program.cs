5112bfff898887ebcaa962f28a22d2ac9f870d08
Carina Rebeca <carina.plisco@gmail.com>
2020-12-20 00:11:25 +0200

    problema 11 - oglindit

---
+++ problema 11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Afisati in ordine inversa cifrele unui numar n. 
+
+            int n, ogl;
+
+            Console.WriteLine("Introduceti un numar n pentru a afisa oglinditul lui.");
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            if(n == 0)
+            {
+                Console.WriteLine($"Oglinditul numarului {n} este 0.");
+            }
+
+            ogl = 0;
+
+            while(n > 0)
+            {
+                ogl = ogl * 10 + n % 10;
+                n = n / 10;
+                
+            }
+            Console.WriteLine($"Oglinditul numarului este {ogl}.");
+        }
+    }
+}
