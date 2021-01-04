dfcb8e550f5446aa9464403da8b0809bf401c33d
Carina Rebeca <carina.plisco@gmail.com>
2021-01-04 15:51:24 +0200

    ghicire nr intre 1 si 1024

---
+++ problema 21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ghiciti un numar intre 1 si 1024 prin intrebari de forma "numarul este mai mare sau egal decat x?". 
+            int x, nr;
+            Random rnd = new Random(); //initializ generator de nr
+
+            x = rnd.Next(1, 1024); //genereaza nr intre 1 si 1024
+
+            do
+            {
+                Console.WriteLine(" Dati un numar intre 1 si 1024");
+                nr = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(" Numarul este mai mare sau egal decat x?");
+
+                if (nr > x)
+                    Console.WriteLine("Da");
+                if (nr < x)
+                    Console.WriteLine("Nu");
+            }
+            while (nr != x);
+
+            Console.WriteLine($" Numarul selectat de calculator este {x}. ");
+        }
+    }
+}
